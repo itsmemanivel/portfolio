9280eee2478ccec735d7b2e0dd156b93dc2320d4
veluvijay <veluvijay@gmail.com>
2019-06-19 21:04:49 +0530

    first commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,142 +1,136 @@
-// Get things set up
-// -------------------------------------------------------------------
-// Include Gulp
-var gulp                    = require("gulp"),
+"use strict";
 
-    // HTML plugins
-    fileinclude             = require("gulp-file-include"),
-    htmlmin                 = require("gulp-htmlmin"),
+// Load plugins
+const autoprefixer = require("gulp-autoprefixer");
+const browsersync = require("browser-sync").create();
+const cleanCSS = require("gulp-clean-css");
+const del = require("del");
+const gulp = require("gulp");
+const header = require("gulp-header");
+const merge = require("merge-stream");
+const plumber = require("gulp-plumber");
+const rename = require("gulp-rename");
+const sass = require("gulp-sass");
+const uglify = require("gulp-uglify");
 
-    // CSS plugins
-    sass                    = require("gulp-sass"),
-    combineMediaQueries     = require("gulp-combine-mq"),
-    autoprefixer            = require("gulp-autoprefixer"),
-    cssmin                  = require("gulp-clean-css"),
-    rename                  = require("gulp-rename"),
-    globber                 = require('glob'),
+// Load package.json for banner
+const pkg = require('./package.json');
 
-    // JS plugins
-    concat                  = require("gulp-concat"),
-    uglify                  = require("gulp-uglify"),
+// Set the banner content
+const banner = ['/*!\n',
+  ' * Start Bootstrap - <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n',
+  ' * Copyright 2013-' + (new Date()).getFullYear(), ' <%= pkg.author %>\n',
+  ' * Licensed under <%= pkg.license %> (https://github.com/BlackrockDigital/<%= pkg.name %>/blob/master/LICENSE)\n',
+  ' */\n',
+  '\n'
+].join('');
 
-    // Image plugin
-    imagemin                = require("gulp-imagemin"),
+// BrowserSync
+function browserSync(done) {
+  browsersync.init({
+    server: {
+      baseDir: "./"
+    },
+    port: 3000
+  });
+  done();
+}
 
-    // General plugins
-    gutil                   = require("gulp-util"),
-    plumber                 = require("gulp-plumber"),
-    size                    = require("gulp-size"),
-    watch                   = require("gulp-watch"),
-    browserSync             = require("browser-sync"),
-    reload                  = browserSync.reload;
+// BrowserSync reload
+function browserSyncReload(done) {
+  browsersync.reload();
+  done();
+}
 
-// Tasks
-// -------------------------------------------------------------------
-// Start server
-gulp.task("browser-sync", function() {
-    browserSync({
-        server: {
-            baseDir: "dist"
-        }
-    });
-});
+// Clean vendor
+function clean() {
+  return del(["./vendor/"]);
+}
 
-// Notify on error with a beep
-var onError = function(error) {
-    console.log(gutil.colors.red(error.message));
-    // https://github.com/floatdrop/gulp-plumber/issues/17
-    this.emit("end");
-    gutil.beep();
-};
-
-// HTML task
-gulp.task("html", function() {
-    return gulp.src("src/html/*.html")
-        // Prevent gulp.watch from crashing
-        .pipe(plumber(onError))
-        // Set up HTML templating
-        .pipe(fileinclude({
-            prefix: "@@",
-            basepath: "src/html"
-        }))
-        // Clean up HTML a little
-        .pipe(htmlmin({
-            removeCommentsFromCDATA: true,
-            removeRedundantAttributes: true,
-            removeEmptyAttributes: true,
-            removeScriptTypeAttributes: true,
-            removeStyleLinkTypeAttributes: true,
-            caseSensitive: true,
-            minifyCSS: true
-        }))
-        // Where to store the finalized HTML
-        .pipe(gulp.dest("dist"));
-});
+// Bring third party dependencies from node_modules into vendor directory
+function modules() {
+  // Bootstrap JS
+  var bootstrapJS = gulp.src('./node_modules/bootstrap/dist/js/*')
+    .pipe(gulp.dest('./vendor/bootstrap/js'));
+  // Font Awesome CSS
+  var fontAwesomeCSS = gulp.src('./node_modules/@fortawesome/fontawesome-free/css/**/*')
+    .pipe(gulp.dest('./vendor/fontawesome-free/css'));
+  // Font Awesome Webfonts
+  var fontAwesomeWebfonts = gulp.src('./node_modules/@fortawesome/fontawesome-free/webfonts/**/*')
+    .pipe(gulp.dest('./vendor/fontawesome-free/webfonts'));
+  // jQuery Easing
+  var jqueryEasing = gulp.src('./node_modules/jquery.easing/*.js')
+    .pipe(gulp.dest('./vendor/jquery-easing'));
+  // jQuery
+  var jquery = gulp.src([
+      './node_modules/jquery/dist/*',
+      '!./node_modules/jquery/dist/core.js'
+    ])
+    .pipe(gulp.dest('./vendor/jquery'));
+  return merge(bootstrapJS, fontAwesomeCSS, fontAwesomeWebfonts, jquery, jqueryEasing);
+}
 
 // CSS task
-gulp.task("css", function() {
-    return gulp.src("src/scss/main.scss")
-        // Prevent gulp.watch from crashing
-        .pipe(plumber(onError))
-        // Compile Sass
-        .pipe(sass({ style: "compressed", noCache: true }))
-        // Combine media queries
-        .pipe(combineMediaQueries())
-        // parse CSS and add vendor-prefixed CSS properties
-        .pipe(autoprefixer({
-            browsers: ["last 2 versions"]
-        }))
-        // Minify CSS
-        .pipe(cssmin())
-        // Rename the file
-        .pipe(rename("production.css"))
-        // Show sizes of minified CSS files
-        .pipe(size({ showFiles: true }))
-        // Where to store the finalized CSS
-        .pipe(gulp.dest("dist/css"));
-});
+function css() {
+  return gulp
+    .src("./scss/**/*.scss")
+    .pipe(plumber())
+    .pipe(sass({
+      outputStyle: "expanded",
+      includePaths: "./node_modules",
+    }))
+    .on("error", sass.logError)
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
+    .pipe(header(banner, {
+      pkg: pkg
+    }))
+    .pipe(gulp.dest("./css"))
+    .pipe(rename({
+      suffix: ".min"
+    }))
+    .pipe(cleanCSS())
+    .pipe(gulp.dest("./css"))
+    .pipe(browsersync.stream());
+}
 
 // JS task
-gulp.task("js", function() {
-    return gulp.src("src/js/**/*")
-        // Prevent gulp.watch from crashing
-        .pipe(plumber(onError))
-        // Concatenate all JS files into one
-        .pipe(concat("production.js"))
-        // Minify JS
-        .pipe(uglify())
-        // Where to store the finalized JS
-        .pipe(gulp.dest("dist/js"));
-});
+function js() {
+  return gulp
+    .src([
+      './js/*.js',
+      '!./js/*.min.js'
+    ])
+    .pipe(uglify())
+    .pipe(header(banner, {
+      pkg: pkg
+    }))
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest('./js'))
+    .pipe(browsersync.stream());
+}
 
-// Image task
-gulp.task("images", function() {
-    return gulp.src("src/img/**/*.+(png|jpeg|jpg|gif|svg)")
-        // Prevent gulp.watch from crashing
-        .pipe(plumber(onError))
-        // Minify the images
-        .pipe(imagemin())
-        // Where to store the finalized images
-        .pipe(gulp.dest("dist/img"));
-});
+// Watch files
+function watchFiles() {
+  gulp.watch("./scss/**/*", css);
+  gulp.watch(["./js/**/*", "!./js/**/*.min.js"], js);
+  gulp.watch("./**/*.html", browserSyncReload);
+}
 
-// Use default task to launch BrowserSync and watch all files
-gulp.task("default", ["browser-sync"], function () {
-    // All browsers reload after tasks are complete
-    // Watch HTML files
-    watch("src/html/**/*", function () {
-        gulp.start("html", reload);
-    });
-    // Watch Sass files
-    watch("src/scss/**/*", function () {
-        gulp.start('css', reload);
-    });
-    // Watch JS files
-    watch("src/js/**/*", function () {
-        gulp.start("js", reload);
-    });
-    // Watch image files
-    watch("src/img/**/*.+(png|jpeg|jpg|gif|svg)", function () {
-        gulp.start("images", reload);
-    });
-});
+// Define complex tasks
+const vendor = gulp.series(clean, modules);
+const build = gulp.series(vendor, gulp.parallel(css, js));
+const watch = gulp.series(build, gulp.parallel(watchFiles, browserSync));
+
+// Export tasks
+exports.css = css;
+exports.js = js;
+exports.clean = clean;
+exports.vendor = vendor;
+exports.build = build;
+exports.watch = watch;
+exports.default = build;
